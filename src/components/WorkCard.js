a9a08d92f7d1e379aff7341efc93e44d6dc56322
Eaucram <164542445+Eaucram@users.noreply.github.com>
2024-03-22 18:52:53 +0000

    Changement @babel

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -7,7 +7,7 @@ import React from 'react'
 const WorkCard = (props) => {
   return (
             <div className="project-card">
-                <img src={props.imgsrc} />
+                <img src={props.imgsrc} alt="" />
                 <h2 className="project-title">{props.title}</h2>
                 <div className="pro-details">
                     <p>{props.text}</p>
